22d80be534fa2d59c0be82ad50f4570ed21116e6
RafaelShift <rafaelfloisl@gmail.com>
2022-02-05 16:13:39 -0600

    Update

--- src/ecuacionsegundogrado/EcuacionSegundoGrado.java
+++ src/ecuacionsegundogrado/EcuacionSegundoGrado.java
@@ -1,38 +1,38 @@
 package ecuacionsegundogrado;
 
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class EcuacionSegundoGrado {
 	static Double a;
 	static Double b;
 	static Double c;
 	static Double raiz;
+	static Double determinante;
 	static Double resultadoPositivo;
 	static Double resultadoNegativo;
 	
+	public static boolean determinante() {
+		determinante = Math.pow(b, 2) - 4 * a * c;
+		boolean valor = (determinante < 0) ? false : true;
+		return valor;
+	}//FIN METODO
+	
 	public static void valoresEcuacion(Double a, Double b, Double c) {
-		
-		raiz = (Math.sqrt(Math.pow(b, 2) - 4 * a * c));
-		
-		if (raiz < 0) {
+		if (!determinante()) {
 			System.out.println("La ecuacion no tiene solución");
 		} else {
-			resultadoPositivo = (((b) * -1.0) + raiz) / (2*a);
-			resultadoNegativo = (((b) * -1.0) - raiz) / (2*a);
+			resultadoPositivo = (((b) * -1.0) + Math.sqrt(determinante)) / (2*a);
+			resultadoNegativo = (((b) * -1.0) - Math.sqrt(determinante)) / (2*a);
 			System.out.println("x1: " + resultadoPositivo);
 			System.out.println("x2: " + resultadoNegativo);
-		}//FIN ELSE 
+		}//FIN ELSE
+
 	}//FIN METODO
 		
 	public static void main (String []args) {
-		Scanner entrada = new Scanner(System.in);
-		System.out.println("Ingresa el valor para a: ");
-		a = entrada.nextDouble();
-		System.out.println("Ingresa el valor para b: ");
-		b = entrada.nextDouble();
-		System.out.println("Ingresa el valor para c: ");
-		c = entrada.nextDouble();
-		
+		a = Double.parseDouble(JOptionPane.showInputDialog("Ingresa el valor para a: "));
+		b = Double.parseDouble(JOptionPane.showInputDialog("Ingresa el valor para b: "));
+		c = Double.parseDouble(JOptionPane.showInputDialog("Ingresa el valor para c: "));
 		valoresEcuacion(a, b, c);
 	}//FIN METODO
 }//FIN CLASE
